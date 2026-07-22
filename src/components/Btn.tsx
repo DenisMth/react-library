8632783e5f7a395bb4b211c11be97acc43460712
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-07-22 12:28:29 +0200

    feat(btn): add component and stories

---
+++ src/components/Btn.tsx
@@ -0,0 +1,20 @@
+type BtnType = {
+    onClick: () => void;
+    children: React.ReactNode;
+}
+
+type BtnProps = {
+    type: 'primary' | 'secondary';
+    btn: BtnType;
+}
+
+export default function Btn({ type, btn }: BtnProps) {
+    return (
+        <button
+            className={`btn ${type}`}
+            onClick={btn.onClick}
+        >
+            {btn.children}
+        </button>
+    );
+}
