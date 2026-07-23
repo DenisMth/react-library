a5f24e54ad45e71cbcba5670224bdaf9ef283167
Denis Mathieu <d.mathieu@stag.technofuturtic.education>
2026-07-23 15:26:33 +0200

    bad link handling fix

--- src/components/Card.stories.tsx
+++ src/components/Card.stories.tsx
@@ -18,7 +18,8 @@ export const Default: Story = {
             title: 'Test',
             subtitle: 'Test2',
             content: 'Wow ! Amazing content !',
-            img: 'https://hips.hearstapps.com/hmg-prod/images/c33b2259-8c6b-4308-bc6f-5373d8a6600d.jpeg?crop=0.668xw:1.00xh;0.194xw,0&resize=640:*'
+            img: 'https://hips.hearstapps.com/hmg-prod/images/c33b2259-8c6b-4308-bc6f-5373d8a6600d.jpeg?crop=0.668xw:1.00xh;0.194xw,0&resize=640:*',
+            buttonLink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
         },
         btnText: 'More',
     },
