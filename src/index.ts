c22ca5e482df753e16c7a7f9530e12067ca82b87
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-07-23 16:25:30 +0200

    feat(index): export components

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { default as Btn } from "./components/Btn";
+export { default as Card } from "./components/Card";
+export { default as Form } from "./components/Form";
+export { default as Field } from "./components/Field";
