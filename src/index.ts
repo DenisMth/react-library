89c36c135b77a29f6c56b018436d27170d78746d
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-07-23 16:56:48 +0200

    refactor(index): import component styles

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import "./index.css";
+import "./components/Styles.css";
+import "./components/Card.css";
+import "./components/Btn.css";
+import "./components/Form.css";
+import "./components/Field.css";
+
 export { default as Btn } from "./components/Btn";
 export { default as Card } from "./components/Card";
 export { default as Form } from "./components/Form";
